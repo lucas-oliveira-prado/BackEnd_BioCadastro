57ee6e9c0b91bcbd4f5eed753c15c4f4c74f6e12
Lucas Prado <83792935+lucas-oliveira-prado@users.noreply.github.com>
2025-06-11 21:48:17 -0300

    atualizado conexão com o banco

--- db.py
+++ db.py
@@ -1,8 +1,18 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:PUC%40123@localhost:3306/biocadastro"
+# Pegando os dados do ambiente do Railway
+DB_USER = os.getenv("MYSQLUSER")
+DB_PASSWORD = os.getenv("MYSQLPASSWORD")
+DB_HOST = os.getenv("MYSQLHOST")
+DB_PORT = os.getenv("MYSQLPORT", "3306")
+DB_NAME = os.getenv("MYSQLDATABASE")
 
+# Montando a URL de conexão corretamente
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Criando engine e session
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
