5afffc1792e038f2072a62199de12d5ec3af8efb
Lucas Prado <83792935+lucas-oliveira-prado@users.noreply.github.com>
2025-06-14 20:34:53 -0300

    Update db.py

--- db.py
+++ db.py
@@ -4,8 +4,6 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
-
 # Criando engine e session
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
