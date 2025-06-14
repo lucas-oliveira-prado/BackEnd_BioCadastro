552496b09f9997573d51eddf2f9a0e9960c2e075
Lucas Prado <83792935+lucas-oliveira-prado@users.noreply.github.com>
2025-06-14 20:12:36 -0300

    Update db.py

--- db.py
+++ db.py
@@ -2,18 +2,10 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Pegando os dados do ambiente do Railway
-DB_USER = os.getenv("MYSQLUSER")
-DB_PASSWORD = os.getenv("MYSQLPASSWORD")
-DB_HOST = os.getenv("MYSQLHOST")
-DB_PORT = os.getenv("MYSQLPORT", "3306")
-DB_NAME = os.getenv("MYSQLDATABASE")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Montando a URL de conexão corretamente
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+engine = create_engine(DATABASE_URL)
 
 # Criando engine e session
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
