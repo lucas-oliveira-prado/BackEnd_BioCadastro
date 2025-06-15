afe15aba94296cf34159414d32ccb5e2414e0e82
Lucas Prado <83792935+lucas-oliveira-prado@users.noreply.github.com>
2025-06-15 20:43:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -104,6 +104,7 @@ if __name__ == "__main__":
     import uvicorn
     import os
     uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+    print(f"-> PORT: {int(os.getenv("PORT", 8000))}")
     
 
 
