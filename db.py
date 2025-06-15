69eedf98a094d08ba4a7d10149aec4253d8b89c6
lucas-oliveira-prado <lucasoliveiraprado2@gmail.com>
2025-06-14 21:06:30 -0300

    corrigido db.py

--- db.py
+++ db.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
+print("DATABASE_URL ->", DATABASE_URL)
 # Criando engine e session
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
